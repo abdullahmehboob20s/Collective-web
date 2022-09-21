f2afa0a7daabba839c7b7133af617dd02fc93dd3
abdullahmehboob20s <abdullahmehboob2002@gmail.com>
2022-09-21 09:44:52 +0500

    initialized codebase

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      spacing: {
+        "1vw": "1vw",
+        "1_2vw": "1.2vw",
+        "1_4vw": "1.4vw",
+        "1_6vw": "1.6vw",
+        "1_8vw": "1.8vw",
+        "2vw": "2vw",
+        "2_2vw": "2.2vw",
+        "2_4vw": "2.4vw",
+        "2_6vw": "2.6vw",
+        "2_8vw": "2.8vw",
+        "3vw": "3vw",
+        "4vw": "4vw",
+        "5vw": "5vw",
+        "6vw": "6vw",
+        "7vw": "7vw",
+      },
+    },
+  },
+  plugins: [],
+};
