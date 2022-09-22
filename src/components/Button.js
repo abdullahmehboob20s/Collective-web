ca5b33c251beee91654f64366a766d9431e13b02
abdullahmehboob20s <abdullahmehboob2002@gmail.com>
2022-09-22 19:57:23 +0500

    added responsive padding

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Button({ title }) {
   return (
-    <button className="fs-16px font-normal border-[1px] border-white rounded-full py-1vw px-2_8vw leading-1">
+    <button className="fs-16px font-normal border-[1px] border-white rounded-full py-2 lg:py-1vw px-6 lg:px-2_8vw leading-1">
       {title}
     </button>
   );
