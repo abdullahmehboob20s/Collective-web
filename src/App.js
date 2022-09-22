446abb16f0c2ce9ced700e8396681c354c920755
abdullahmehboob20s <abdullahmehboob2002@gmail.com>
2022-09-22 15:07:31 +0500

    added member section and customized margins

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import Comitted from "layouts/Comitted";
 import Hero from "layouts/Hero";
+import Membership from "layouts/Membership";
 import Navbar from "layouts/Navbar";
 
 function App() {
   return (
     <div>
       <Navbar />
-      <div className="mb-4vw">
+      <div className="mb-50px">
         <Hero />
       </div>
-      <div className="mb-6vw">
+      <div className="mb-150px">
         <Comitted />
+      </div>
+      <div className="mb-150px">
+        <Membership />
       </div>
     </div>
   );
