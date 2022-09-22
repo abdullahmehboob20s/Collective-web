0dd604adde81d3423b7a2f45ac7668d01585ab59
abdullahmehboob20s <abdullahmehboob2002@gmail.com>
2022-09-22 15:42:29 +0500

    replaced text with component

--- src/layouts/Comitted.js
+++ src/layouts/Comitted.js
@@ -1,26 +1,26 @@
+import HeadingAndTitle from "components/HeadingAndTitle";
 import React from "react";
 
 function Comitted() {
   return (
     <div className="container-wrapper">
-      <div className="max-w-[90%]">
-        <p className="fs-44px font-light mb-3vw leading-1_3">
-          We are committed to creating a workspace that fosters creativity,
-          improves energy, lifts mood and increases overall wellbeing and
-          productivity{" "}
-          <a href="/" className="text-blue">
-            in Richmond, Melbourne
-          </a>
-          .
-        </p>
-        <div className="flex justify-end">
-          <p className="fs-18px font-light max-w-[18em]">
+      <HeadingAndTitle
+        heading={
+          <>
+            We are committed to creating a workspace that fosters creativity,
+            improves energy, lifts mood and increases overall wellbeing and
+            productivity{" "}
+            <span className="text-blue">in Richmond, Melbourne</span>.
+          </>
+        }
+        title={
+          <>
             Our goal is to bring people back into the office, not by force, but
             by creating a space where people can thrive, grow and form real
             connections. A vibrant, productive offices space is our main mission
-          </p>
-        </div>
-      </div>
+          </>
+        }
+      />
     </div>
   );
 }
