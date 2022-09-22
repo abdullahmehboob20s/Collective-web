d2f1b501947d455cb8a165f380bcaf0e15176cdc
abdullahmehboob20s <abdullahmehboob2002@gmail.com>
2022-09-22 15:21:26 +0500

    section created

---
+++ src/layouts/CarefullyDesigned.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+function CarefullyDesigned() {
+  return (
+    <div>
+      <div className="container-wrapper">
+        <div className="flex justify-end mb-4vw">
+          <h1 className="fs-44px font-light leading-1_3 max-w-[91%]">
+            Through a thought process of careful consideration, we have
+            carefully structured a range of spaces,{" "}
+            <span className="text-blue">
+              each space carefully designed to compliment different moods and
+              tasks
+            </span>
+            .
+          </h1>
+        </div>
+
+        <p className="fs-16px font-light max-w-[20em]">
+          We know not all work happens the same, sometime you need a space to
+          collaborate, other times a desk is completely appropriate, no one
+          always needs the same.{" "}
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default CarefullyDesigned;
