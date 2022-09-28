b6567201431e602a9804abe09a94638c93465b53
abdullahmehboob20s <abdullahmehboob2002@gmail.com>
2022-09-28 16:59:41 +0500

    passing props

--- src/components/TitleBar.js
+++ src/components/TitleBar.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Button from "./Button";
 
-function TitleBar({ title }) {
+function TitleBar({ title, buttonShow = true }) {
   return (
     <div className="flex flex-col sm:flex-row items-start space-y-5 sm:space-y-0 sm:items-center justify-between">
       <h1 className="fs-44px text-white font-light">{title}</h1>
-      <Button title="Enquire" />
+
+      {buttonShow && <Button title="Enquire" />}
     </div>
   );
 }
