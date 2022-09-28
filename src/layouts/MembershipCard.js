0e968c486e30d10cc2d9a7a2d5fad2d6245d3871
abdullahmehboob20s <abdullahmehboob2002@gmail.com>
2022-09-28 17:00:38 +0500

    added props

--- src/layouts/MembershipCard.js
+++ src/layouts/MembershipCard.js
@@ -2,18 +2,30 @@ import CapacityCard from "components/CapacityCard";
 import React from "react";
 import { BsFillPersonFill } from "react-icons/bs";
 
-function MembershipCard({ min, max, title, desc }) {
+function MembershipCard({
+  min,
+  max,
+  title,
+  desc,
+  img,
+  titleBorder = true,
+  imgHeight = "lg:h-[26vw]",
+}) {
   return (
     <div className="">
       <div className="mb-6 lg:mb-1_4vw">
         <img
-          src="images/hero-banner.png"
-          className="w-full object-cover lg:h-[26vw]"
+          src={img ? img : "images/hero-banner.png"}
+          className={`w-full object-cover ${imgHeight}`}
           alt=""
         />
       </div>
 
-      <div className="flex items-center space-x-4 lg:space-x-1_2vw pb-3 sm:pb-4 lg:pb-1vw border-b-2 border-white mb-4 lg:mb-1vw">
+      <div
+        className={`flex items-center space-x-4 lg:space-x-1_2vw ${
+          titleBorder && "border-b-2 border-white pb-3 sm:pb-4 lg:pb-1vw"
+        } mb-4 lg:mb-1vw`}
+      >
         <p className="fs-20px font-light">{title}</p>
         {!min || !max ? null : (
           <CapacityCard
