a95558f44ff3d78e5a63eb043ec6e28c5ffc1077
abdullahmehboob20s <abdullahmehboob2002@gmail.com>
2022-09-29 11:27:46 +0500

    fixed scaling issue

--- src/layouts/Gallery.js
+++ src/layouts/Gallery.js
@@ -4,9 +4,9 @@ function Gallery() {
   return (
     <div>
       <div className="container-wrapper grid grid-cols-2 gap-3vw">
-        <img src="images/grid-img-1.png" alt="" />
-        <img src="images/grid-img-2.png" alt="" />
-        <img src="images/grid-img-3.png" className="col-span-2" alt="" />
+        <img src="images/grid-img-1.png" className="w-full" alt="" />
+        <img src="images/grid-img-2.png" className="w-full" alt="" />
+        <img src="images/grid-img-3.png" className="w-full col-span-2" alt="" />
       </div>
     </div>
   );
